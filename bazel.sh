224f5874c07001d8fa7d8aedc09efa2fd73cf75f
markchen0308 <simonchen0308@gmai.com>
2018-12-09 20:03:45 +0800

    f

--- bazel.sh
+++ bazel.sh
@@ -29,7 +29,7 @@ grep -Rl 'lib64' | xargs sed -i 's/lib64/lib/g'
 sudo blkid 
 #write uuid
 sudo nano /etc/fstab
-UUID=XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX none swap sw,pri=5 0 0
+UUID=3683135b-b251-4343-86d8-73e5e5806ef6 none swap sw,pri=5 0 0
 
  sudo swapon -a  #enable swap
 
@@ -48,13 +48,34 @@ UUID=XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX none swap sw,pri=5 0 0
       build_file = str(Label("//third_party:eigen.BUILD")),
   )
 
+./configure
+Please specify the location of python. [Default is /usr/bin/python]: /usr/bin/python3
+
+
+Found possible Python library paths:
+  /usr/local/lib/python3.5/dist-packages
+  /usr/lib/python3/dist-packages
+Please input the desired Python library path to use.  Default is [/usr/local/lib/python3.5/dist-packages]
+
+Do you wish to build TensorFlow with jemalloc as malloc support? [Y/n]: Y
+jemalloc as malloc support will be enabled for TensorFlow.
+Do you wish to build TensorFlow with Google Cloud Platform support? [Y/n]: n
+No Google Cloud Platform support will be enabled for TensorFlow.
+
+Do you wish to build TensorFlow with Hadoop File System support? [Y/n]: n
+No Hadoop File System support will be enabled for TensorFlow.
+
+Do you wish to build TensorFlow with Amazon S3 File System support? [Y/n]: n
+No Amazon S3 File System support will be enabled for TensorFlow.
+
+
 
 #build
 bazel build -c opt --copt="-mfpu=neon-vfpv4" --copt="-funsafe-math-optimizations" --copt="-ftree-vectorize" --copt="-fomit-frame-pointer" --local_resources 1024,1.0,1.0 --verbose_failures --incompatible_load_argument_is_label=false tensorflow/tools/pip_package:build_pip_package
 #package
 bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
 #install
-sudo pip3 install /tmp/tensorflow_pkg/tensorflow-1.4.1-cp35-cp35m-linux_armv7l.whl
+sudo pip3 install /tmp/tensorflow_pkg/tensorflow-1.4.1-cp35-cp35m-linux_armv7l.whl --user
 #disable swap
 sudo swapoff /dev/sdb1
 #delete uuid
