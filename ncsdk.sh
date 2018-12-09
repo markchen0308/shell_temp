2baf1eb1194ace55486a3dfc44337c7fe471c79c
markchen0308 <simonchen0308@gmai.com>
2018-12-09 22:56:45 +0800

    aa

--- ncsdk.sh
+++ ncsdk.sh
@@ -4,3 +4,9 @@ cd ncsdk
 gedit ncsdk.conf
 make install
 
+python-dev
+sudo apt-get install  graphviz libgraphviz-dev pkg-config
+pip3 install pygraphviz --user
+
+python3-markdown python3-h5py python3-lxml python3-matplotlib 
+python3-protobuf python3-dateutil python3-scipy python3-six pyth$
